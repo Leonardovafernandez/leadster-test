0ccfc9099fea47b9601174d569e910047e162123
unknown <leonardovafernandez@gmail.com>
2023-06-28 22:38:55 -0300

    Estruturação e estilização da section 'CTA'

--- leadster-page/src/app/page.js
+++ leadster-page/src/app/page.js
@@ -1,7 +1,11 @@
 import Image from 'next/image';
 import Header from './../components/Header'
 import styles from "./page.module.css";
-import effect from "../assets/asset-header.png"
+import effect from "../assets/asset-header.png";
+import ctaImg from "../assets/comparativo_img_CTA.png";
+import rating from "../assets/rating.webp";
+import noCard from "../assets/no-card-dark.webp";
+import selo from "../assets/selo_RD.png";
 
 export default function Home() {
   return (
@@ -24,7 +28,26 @@ export default function Home() {
           <p>LIBRARY</p>
         </section>
         <section >
-
+          <div className={styles.cta}>
+            <Image className={styles.ctaImg} src={ctaImg} />
+            <div className={styles.ctaContent}>
+              <h3>Pronto para triplicar sua</h3>
+              <h3><strong>Geração de Leads?</strong></h3>
+              <h4>Criação e ativação em <strong>4 minutos.</strong></h4>
+              <div className={styles.boxTop}>
+                <div className={styles.button}>
+                  <button><strong>VER DEMOSTRAÇÃO</strong></button>
+                  <Image src={selo} />
+                </div>
+                <div>
+                  <Image src={noCard} />
+                  <span> Não é necessário Cartão de Crédito  | </span>
+                  <Image src={rating} />
+                  <span><strong> 4.9</strong>/5 média de satisfação</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
       <footer>
---
+++ leadster-page/src/components/Card/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import logo from "./../../assets/logo.png";
+import Image from "next/image";
+
+export default function Card() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
